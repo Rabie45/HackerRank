5a633474b0a1b7719cc0bf8078b6b740d007c757
Abdallah Rabie <76526170+Rabie45@users.noreply.github.com>
2023-01-09 12:19:26 -0800

    Update Calculate the Nth term.c

--- Calculate the Nth term/Calculate the Nth term.c
+++ Calculate the Nth term/Calculate the Nth term.c
@@ -5,20 +5,14 @@
 //Complete the following function.
 
 int find_nth_term(int n, int a, int b, int c) {
-    
-    if (a<=100 && a>=1 ) {
-    return 1;
-    }
-    else if (b<=100 && b>=1 ) {
-    
-    return 2;
-    }
-    else if ( c<=100 && c>=1) {
-    
-    return 3;
-    }
+    if(n==1)
+    return a;
+    else if(n==2)
+    return b;
+    else if (n==3) 
+    return c;
     else {
-    return (find_nth_term(n--,a,b,c));
+    return (find_nth_term(n-1,a,b,c)+find_nth_term(n-2,a,b,c)+find_nth_term(n-3,a,b,c));
     }
   //Write your code here.
 }
